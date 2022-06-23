4abccc2efa493169cf8f2b253f258ba41ea117bf
Brandon Ines <brandonines@Brandons-MBP.attlocal.net>
2022-06-23 17:20:15 -0500

    added Contact info to Sanity

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,26 +1,43 @@
-
+import React, { useState, useEffect } from "react";
+import sanityClient from "../client.js";
 
 export default function Contact() {
+    const [contactData, setContact] = useState(null);
+
+    useEffect(() => {
+        sanityClient
+        .fetch(
+            `*[_type == "contact"]{
+                    heading,
+                    message,
+                    email,                  
+                }`
+        )
+        .then((data) => setContact(data))
+        .catch(console.error);
+    }, []);
+
     return (
         <main>
             <section className="relative flex justify-center min-h-screen bg-black pt-12 lg:pt-64 lg:px-80 px-32">
-                <div className="text-center w-8/12 2xl:px-16">
+                {contactData && contactData.map((contact, index) => (
+                    <div className="text-center w-8/12 2xl:px-16" key={index}>
                     <h1 className="text-6xl text-neutral-50 lora leading-none lg:leading-snug">
-                        Get In Touch
+                        {contact.heading}
                     </h1>
                     <p className="lato text-neutral-200 font-light my-3 text-lg 2xl:px-16">
-                        I'm currently looking for new opportunities.  Feel free to reach out for any inquiries or even just to say hello.
+                        {contact.message}
                     </p>
                     <a
-                    href="mailto:brandon.ines20@gmail.com" 
+                    href={contact.email}
                     rel="noopener noreferrer" 
                     >
                         <button className="bg-transparent hover:bg-orange-300 text-orange-300 lato hover:bg-opacity-20 py-2 px-4 border border-orange-300 transition delay-85 duration-300 ease-in-out rounded my-3">
                             Say Hello
                         </button>
                     </a>
-                    
-                </div>
+                    </div>
+                ))}
                 
             </section>
         </main>
---
+++ studio-portfolio/schemas/contact.js
@@ -0,0 +1,23 @@
+export default {
+    name: 'contact',
+    title: 'Contact',
+    type: 'document',
+    fields: [
+        {
+            name: 'heading',
+            title: 'Heading',
+            type: 'text',
+        },
+        {
+            name: 'message',
+            title: 'Message',
+            type: 'text',
+        },
+        {
+            name: 'email',
+            title: 'Email',
+            type: 'string',
+        },
+    ],
+  }
+  
